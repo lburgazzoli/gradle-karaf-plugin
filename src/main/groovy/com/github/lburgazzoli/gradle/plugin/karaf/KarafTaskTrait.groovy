e5bde466cc554ca57833df3c9a033bb579faacd4
lburgazzoli <lburgazzoli@gmail.com>
2016-08-30 09:22:49 +0200

    add base trait to tasks

---
+++ src/main/groovy/com/github/lburgazzoli/gradle/plugin/karaf/KarafTaskTrait.groovy
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2016, Luca Burgazzoli and contributors as indicated by the @author tags
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.lburgazzoli.gradle.plugin.karaf
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import org.gradle.jvm.tasks.Jar
+
+import com.github.lburgazzoli.gradle.plugin.karaf.KarafPluginExtension
+import com.github.lburgazzoli.gradle.plugin.karaf.mvn.MvnProtocolParser
+
+/**
+ * @author lburgazzoli
+ */
+trait KarafTaskTrait {
+    private KarafPluginExtension pluginExtension;
+
+    void karaf(Closure closure) {
+        this.pluginExtension = ConfigureUtil.configure(
+            closure,
+            new KarafPluginExtension()
+        )
+    }
+
+    void karaf(KarafPluginExtension pluginExtension) {
+        this.pluginExtension = pluginExtension
+    }
+
+    KarafPluginExtension getKaraf() {
+        // Don't keep looking it up...
+        if (this.pluginExtension == null) {
+            this.pluginExtension = KarafPluginExtension.lookup(project)
+        }
+
+        return this.pluginExtension;
+    }
+}
